57a826178d1972268154e5b8dffa18d80b8f036b
Guyo <guyo.halake@students.moringaschool.com>
2023-11-17 20:55:23 +0300

    backend

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
 
 function App() {
   return (
     <div>
+      <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={< Login/> } />
+        <Route path="/signup" element={< Signup/> } />
+        <Route path="/account" element={< Account/> } />
       </Routes>
+      </AuthContextProvider>
+
     </div>
   );
 }
