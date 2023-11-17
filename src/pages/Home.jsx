3fa164eff0b570ce81067b2b65ef9fa780032926
Guyo <guyo.halake@students.moringaschool.com>
2023-11-17 18:48:04 +0300

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,12 @@ function Home() {
   return (
     <div>
         <Main />
-        <Row rowID='1' title='Up coming' fetchURL={requests.requestUpcoming} />
+        <Row rowID='4' title='Trending' fetchURL={requests.requestTrending} />
         <Row rowID='2' title='Popular' fetchURL={requests.requestPopular} />
         <Row rowID='3' title='Top Rated' fetchURL={requests.requestTopRated} />
-        <Row rowID='4' title='Trending' fetchURL={requests.requestTrending} />
+        <Row rowID='1' title='Up coming' fetchURL={requests.requestUpcoming} />
         <Row rowID='5' title='Horror' fetchURL={requests.requestHorror} />
+
     </div>
   )
 }
